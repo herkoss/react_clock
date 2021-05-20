5c9a229f40016e1766a7f79d6d2d797898dbf729
herkoss <dmytroherko@icloud.com>
2021-05-20 11:14:01 +0300

    Updates

---
+++ precache-manifest.b167a2b6d3004a40033107b311c2c5c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e90254a3fbf2b67e3ec82b1a7087af7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e38f8f1529f110d40d03",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "33e27156f4d3fa24d47b",
+    "url": "./static/js/2.021d3507.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.021d3507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e38f8f1529f110d40d03",
+    "url": "./static/js/main.9b497e07.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
